6108ac12d8f00932943c94665e9864e1c4ff0df0
luzilong <luzilong2005@foxmail.com>
2025-10-21 19:21:45 +0800

    更新 Docs 标题

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ import { groupIconMdPlugin, groupIconVitePlugin } from "vitepress-plugin-group-i
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     base: "/modern-ui/",
-    title: "Modern UI",
+    title: "Modern UI Docs",
     description: "使用 Vue + Vite + TypeScript 构建的组件库",
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
